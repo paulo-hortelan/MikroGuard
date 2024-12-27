faed07545c7be5b3e6cc4bbbfa88218e50b7f21a
Paulo <paulo.ribeiro@aonet.com.br>
2024-12-27 15:14:42 -0300

    imp: qrcode info

--- frontend/src/utils/config-string-generator.ts
+++ frontend/src/utils/config-string-generator.ts
@@ -4,11 +4,13 @@ const generateString = (config: Config) => {
 
   return  `
 [Interface]
+#${config.peer_name}
 ListenPort = 51820
 PrivateKey = ${config.peer_private_key}
 Address = ${config.address}
 
 [Peer]
+#${config.server_name}
 PublicKey = ${config.server_public_key}
 AllowedIPs = ${config.allowed_ips}
 Endpoint = ${config.client_endpoint}
