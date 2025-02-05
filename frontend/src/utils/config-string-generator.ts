50b4e8055e972fc5d332ed552a47af9f00a02b1f
Paulo <paulo.ribeiro@aonet.com.br>
2025-02-05 13:51:13 -0300

    fix

--- frontend/src/utils/config-string-generator.ts
+++ frontend/src/utils/config-string-generator.ts
@@ -5,7 +5,7 @@ const generateString = (config: Config) => {
   return  `
 [Interface]
 #${config.peer_name}
-ListenPort = 51820
+ListenPort = 23231
 PrivateKey = ${config.peer_private_key}
 Address = ${config.address}
 DNS = ${config.dns}
@@ -14,7 +14,8 @@ DNS = ${config.dns}
 #${config.server_name}
 PublicKey = ${config.server_public_key}
 AllowedIPs = ${config.allowed_ips}
-Endpoint = ${config.client_endpoint}:13231
+Endpoint = ${config.client_endpoint}:23231
+PersistentKeepalive = 5
 `.trim();
 
 }
